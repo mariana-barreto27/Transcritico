39662da3cb28d56a308a48bfcdcc40793df6027f
Mariana Barreto Da Cunha <141162633+mariana-barreto27@users.noreply.github.com>
2026-07-21 09:15:06 -0300

    mudanca de temp vapor e evaporador
    Mudanca na temperatura de saida da agua, para 90degC e evaporacao para  15degC

--- cycle_model.py
+++ cycle_model.py
@@ -28,9 +28,9 @@ from property_functions import (
 @dataclass(frozen=True)
 class CycleInputs:
     Q_sink: float = 120e3
-    T_steam: float = 120.0 + 273.15
+    T_steam: float = 90.0 + 273.15
     T_feedwater: float = 20.0 + 273.15
-    T_evap: float = 0.0 + 273.15
+    T_evap: float = 15.0 + 273.15
     eta_is_comp: float = 0.70
     eta_motor: float = 0.95
     epsilon_IHX: float = 0.70
