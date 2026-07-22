6d07b2e3fea0c95402fac426f7ef106fad711b1f
Mariana Barreto Da Cunha <141162633+mariana-barreto27@users.noreply.github.com>
2026-07-22 14:16:58 -0300

    Update cycle_model.py

--- cycle_model.py
+++ cycle_model.py
@@ -28,7 +28,7 @@ from property_functions import (
 @dataclass(frozen=True)
 class CycleInputs:
     Q_sink: float = 120e3
-    T_steam: float = 90.0 + 273.15
+    T_steam: float = 120.0 + 273.15
     T_feedwater: float = 20.0 + 273.15
     T_evap: float = 15.0 + 273.15
     eta_is_comp: float = 0.70
